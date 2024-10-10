73c675897dff9bcbffe140e6e7bf6f87b8068381
parmag-1ucas <lucas.parmagnani01@gmail.com>
2024-10-10 11:25:40 -0300

    Estilização da página com o Bootstrap e Font Awesome

--- index.php
+++ index.php
@@ -9,9 +9,13 @@ require 'db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Sistema de Vendas</title>
+    <script src="https://kit.fontawesome.com/f2f4add29b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Sistema de Vendas - Pedidos</h1>
+    <div class="container text-center mt-5">    
+        <h1>Sistema de Vendas - Pedidos</h1>
+    </div>
 
     <?php
     if (isset($_POST['remover'])) {
@@ -43,69 +47,75 @@ require 'db.php';
         $stmt->execute();
         $stmt->close();
 
-        echo "<p style='color:green;'>Pedido removido com sucesso!</p>";
+        echo "<p class='alert alert-success'>Pedido removido com sucesso!</p>";
     }
     ?>
+    <div class="container mt-5">
+        <h2 class="text-center">Pedidos Cadastrados</h>
+        <table class="table table-bordered table-striped mt-4">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID do Pedido</th>
+                    <th>Cliente</th>
+                    <th>Quantidade Total</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT * FROM pedidos";
+                $result = $mysqli->query($query);
 
-    <h2>Pedidos Cadastrados</h2>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID do Pedido</th>
-                <th>Cliente</th>
-                <th>Quantidade Total</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $query = "SELECT * FROM pedidos";
-            $result = $mysqli->query($query);
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['cliente'] . "</td>";
+                    echo "<td>" . $row['quantidade_total'] . "</td>";
+                    echo "<td>";
+                    echo "<form action='index.php' method='post' style='display:inline;'>";
+                    echo "<input type='hidden' name='pedido_id' value='" . $row['id'] . "'>";
+                    echo "<button type='submit' name='remover' class='btn btn-link text-danger'><i class='fa fa-trash'></i></button>";
+                    echo "</form>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        <div class="text-center mt-3">
+            <a class="btn btn-dark" href="cadastrar_pedido.php">Cadastrar Novo Pedido</a>
+        </div>
+    </div>
 
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['cliente'] . "</td>";
-                echo "<td>" . $row['quantidade_total'] . "</td>";
-                echo "<td>";
-                echo "<form action='index.php' method='post' style='display:inline;'>";
-                echo "<input type='hidden' name='pedido_id' value='" . $row['id'] . "'>";
-                echo "<button type='submit' name='remover'>Remover Pedido</button>";
-                echo "</form>";
-                echo "</td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+    <div class="container mt-5">
+        <h2 class="text-center">Produtos em Estoque</h2>
+        <table class="table table-bordered table-striped mt-4">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID do Produto</th>
+                    <th>Nome</th>
+                    <th>Estoque</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT * FROM produtos";
+                $result = $mysqli->query($query);
 
-    <h2>Produtos em Estoque</h2>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID do Produto</th>
-                <th>Nome</th>
-                <th>Estoque</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $query = "SELECT * FROM produtos";
-            $result = $mysqli->query($query);
-
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['nome'] . "</td>";
-                echo "<td>" . $row['estoque'] . "</td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <br>
-    <a href="cadastrar_pedido.php">Cadastrar Novo Pedido</a>
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['nome'] . "</td>";
+                    echo "<td>" . $row['estoque'] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        <div class="text-center mt-3">
+            <a class="btn btn-dark" href="cadastrar_produto.php">Cadastrar Novo Produto</a>
+        </div>
+    </div>
 
 </body>
 </html>
